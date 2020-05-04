a8b994108ccfc593c792f7a195558a2598bdd39a
amsabots <amsabots@gmail.com>
2020-05-04 18:20:08 +0300

    more features added

--- app/src/main/java/com/example/suzzy/MainFrags/MoreFrag.java
+++ app/src/main/java/com/example/suzzy/MainFrags/MoreFrag.java
@@ -10,12 +10,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.suzzy.CreateAccount;
+import com.example.suzzy.MoreOptions.Account;
+import com.example.suzzy.MoreOptions.History;
+import com.example.suzzy.MoreOptions.Maps;
 import com.example.suzzy.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 
-public class MoreFrag extends Fragment {
+public class MoreFrag extends Fragment implements View.OnClickListener {
+    private TextInputLayout city, residence, phonenumber;
+    Button update_contact, update_location;
+    TextView open_maps;
+    LinearLayout open_accounts, history;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +38,49 @@ public class MoreFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View view = inflater.inflate(R.layout.fragment_more, container, false);
-
-       return view;
+        View view = inflater.inflate(R.layout.fragment_more, container, false);
+        //initialise all the views on the xml
+        initViews(view);
+//setting the onclick listeners to th views
+        update_location.setOnClickListener(this);
+        update_contact.setOnClickListener(this);
+        open_maps.setOnClickListener(this);
+        open_accounts.setOnClickListener(this);
+        return view;
     }
-    private void showSigninOptions(){}
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fragment_more_update_phonenumber:
+                break;
+            case R.id.fragment_more_update_address:
+                break;
+            case R.id.fragment_get_device_location:
+                startActivity(new Intent(getContext(), Maps.class));
+                break;
+            case R.id.fragment_more_account:
+                startActivity(new Intent(getContext(), Account.class));
+                break;
+            case R.id.fragment_more_history:
+                startActivity(new Intent(getContext(), History.class));
+                break;
+
+        }
+    }
+
+
+    private void initViews(View view) {
+        city = view.findViewById(R.id.fragment_more_current_city);
+        residence = view.findViewById(R.id.fragment_more_current_residence);
+        phonenumber = view.findViewById(R.id.fragment_more_activephone);
+        update_contact = view.findViewById(R.id.fragment_more_update_phonenumber);
+        update_location = view.findViewById(R.id.fragment_more_update_address);
+        open_maps = view.findViewById(R.id.fragment_get_device_location);
+        open_accounts = view.findViewById(R.id.fragment_more_account);
+        history = view.findViewById(R.id.fragment_more_history);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/suzzy/MoreOptions/Maps.java
@@ -0,0 +1,16 @@
+package com.example.suzzy.MoreOptions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.suzzy.R;
+
+public class Maps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
